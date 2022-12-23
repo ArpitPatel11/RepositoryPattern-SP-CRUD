5289b3e351d8ec9545728149deeefc80533958f8
Admin <Admin@ARPITATOZ>
2022-12-23 17:19:06 +0530

    test

--- SPCrudAPI/Models/SubCategory.cs
+++ SPCrudAPI/Models/SubCategory.cs
@@ -10,7 +10,7 @@ namespace SPCrudAPI.Models
         public int CategoryId { get; set; }
         public string CategoryName { get; set; } = string.Empty;
         public int SubCategoryId { get; set; }
-        public string SubCategoryName { get; set; }    = string.Empty;
+        public string SubCategoryName { get; set; } = string.Empty;
         public bool? IsActive { get; set; }
     }
 }
